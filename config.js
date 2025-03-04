966045c1a47a339203aeb579fdf5f4c5a50fcf37
Rohit <rohit_dogra13>
2025-03-04 19:29:56 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+
+var config = {
+    lines: [
+        "Dear Friend,",
+        "I never knew how much you mean to me when we first started talking . until today,i still love you as much as i did since the very first day we met. ",
+        " Every since you came into my life, i felt like i was the happiest. I just want to let you know how much you mean to me and how much i appreciate you.",
+        "You never failed to make me smile. Things aren't getting better every since we fought, I just miss you so much. having random break downs, with you on my mind wondering if you're doing fine. I'm sorry I wasn't what you wanted,",
+        "I'm sorry I wasn't the best, I'm sorry I couldn't be good enough for you.I just hope everything will be okay soon, I really miss how we used to talk.🥹", 
+       
+    ],
+    date:"2024-05-09T20:15:00",
+    names: ["☃︎【 Your Rohit 】","‎ "]
+}
